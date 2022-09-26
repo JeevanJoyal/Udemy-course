78be14b445c3a4df3bfd3d6cb893049a867eee55
JeevanJoyal <jeevanjoyal77@gmail.com>
2022-09-26 22:49:53 +0530

    day18 Express.js section completed

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { application } = require("express");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/",function(req,res) {
-    res.send("Hello, world!");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    res.send("The result of the calculation is " + (num1+num2));
 });
 
 app.listen(3000,function(){
     console.log("Server listening on port 3000");
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmicalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    res.send("The result of the calculation is " + (weight / (weight*height)));
 });
